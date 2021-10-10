32784ddc0be94c8c7e2f4fd8d7c3d84cdb85381b
Li-Wei.Cheng <r93096@csie.ntu.edu.tw>
2021-10-10 14:47:18 +0800

    leetcode: add dfs approach

--- Leetcode/207.cpp
+++ Leetcode/207.cpp
@@ -32,3 +32,47 @@ public:
         return cnt == i_deg.size();
     }
 };
+
+class Solution {
+public:
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> neighbors(numCourses);
+        for(auto &prerequisite : prerequisites)
+            neighbors[prerequisite[0]].push_back(prerequisite[1]);
+        vector<pair<vector<int>, bool>> ccs;
+        findCCs(neighbors, ccs);
+        return ccs.end() == find_if(ccs.begin(), ccs.end(), [](const pair<vector<int>, bool> &p){ return p.second;});
+    }
+    
+    void findCCs(vector<vector<int>> &neighbors, vector<pair<vector<int>, bool>> &ccs) {
+        vector<bool> visited(neighbors.size(), false);
+        vector<bool> in_call_stack(neighbors.size(), false);
+        for(int i=0; i<visited.size(); i++)
+            if(!visited[i]) {
+                vector<int> cc;
+                bool hasCycle=false;
+                dfsUtil(neighbors, visited, i, &in_call_stack, &cc, hasCycle);
+                ccs.emplace_back(make_pair(cc, hasCycle));
+            }
+    }
+    
+    void dfsUtil(vector<vector<int>> &neighbors, vector<bool> &visited, int cur,
+                 vector<bool> *p_in_call_stack,
+                 vector<int> *p_cc, bool &hasCycle) {
+        if(hasCycle) return;
+        visited[cur] = true;
+        if(p_in_call_stack) (*p_in_call_stack)[cur] = true;
+        for(auto neighbor : neighbors[cur]) {
+            if(p_in_call_stack && (*p_in_call_stack)[neighbor]) {
+                hasCycle=true;
+                return;
+            }
+            if(!visited[neighbor])
+                dfsUtil(neighbors, visited, neighbor, p_in_call_stack, p_cc, hasCycle);
+        }
+        // put it when all children visited
+        if(p_cc) (*p_cc).push_back(cur);
+        if(p_in_call_stack) (*p_in_call_stack)[cur] = false;
+    }
+};
