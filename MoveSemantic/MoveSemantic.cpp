033f6209d69271f34642cddf6d73983569bbf5c3
Li-Wei.Cheng <r93096@csie.ntu.edu.tw>
2021-10-13 21:24:13 +0800

    leetcode: 912. Sort an Array

--- MoveSemantic/MoveSemantic.cpp
+++ MoveSemantic/MoveSemantic.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -100,9 +101,110 @@ void funcMove(const Data&& data) {
 	cout << endl;
 }
 
+
+class Solution {
+public:
+	vector<int> sortArray(vector<int>& nums) {
+		quickSort(nums, 0, nums.size()-1);
+		return nums;
+	}
+
+	/// two pointer
+	int partition(vector<int>& nums, int s, int e) {
+/*
+		int j = s, pi = e;
+		for (int i = s; i < e; i++)
+			if (nums[i] > nums[pi])
+				swap(nums[i], nums[j++]);
+		swap(nums[j], nums[pi]);
+		return j;
+*/
+		cout << __func__ << endl;
+		for (auto num : nums)
+			cout << num << " ";
+		cout << endl;
+		int l = s, r = e, pivot = nums[s];
+		while (l < r) {
+			while (nums[l] < pivot) l++;
+			while (nums[r] > pivot) r--;
+			if (l < r) swap(nums[l++], nums[r--]);
+		}
+		for (auto num : nums)
+			cout << num << " ";
+		cout << endl;
+		cout << "pivot:" << pivot << " l=" << l << endl;
+		return l;
+	}
+
+	void quickSort(vector<int>& nums, int s, int e) {
+		/// s == e means it's sorted, we needn't do anything
+		if (s < e) {
+			int p = partition(nums, s, e);
+			quickSort(nums, s, p-1);
+			quickSort(nums, p + 1, e);
+		}
+	}
+
+	void mergeSortR(vector<int>& nums, int s, int e) {
+		if (s >= e)
+			return;
+		else {
+			int m = s + (e - s) / 2;
+			mergeSortR(nums, s, m);
+			mergeSortR(nums, m + 1, e);
+			mergeTwo(nums, s, m, e);
+		}
+	}
+
+	/// s 
+	/// 0 1 2
+	void mergeSortI(vector<int>& nums) {
+		int len = nums.size();
+		for (int seg = 1; seg < len; seg += seg)
+			for (int start = 0; start < len; start += 2 * seg) {
+				mergeTwo(nums, start, min(start + seg, len-1), min(start + 2 * seg, len-1));
+			}
+	}
+
+	void mergeTwo(vector<int>& nums, int s, int m, int e) {
+		if (m + 1 > e) return;
+
+		int left = 0, right = 0;
+		vector<int> l(nums.begin() + s, nums.begin() + m + 1);
+		vector<int> r(nums.begin() + m + 1, nums.begin() + e + 1);
+		l.push_back(INT_MAX);
+		r.push_back(INT_MAX);
+		for (int i = s; i <= e; i++)
+			if (l[left] < r[right])
+				nums[i] = l[left++];
+			else
+				nums[i] = r[right++];
+
+		/*
+				while(left<l.size() && right<r.size())
+					if(l[left] < r[right])
+						nums[start++] = l[left++];
+					else
+						nums[start++] = r[right++];
+				while(left<l.size())
+					nums[start++] = l[left++];
+				while(right<r.size())
+					nums[start++] = r[right++];
+		*/
+	}
+};
+
 int main()
 {
-	/// 1. pass an instance into a container
+	Solution s;
+	vector<int> data = { -2,3,-5 };
+	s.sortArray(data);
+
+	for (auto i : data)
+		cout << i << " ";
+	cout << endl;
+/*
+	/// 1. pass an instance into a container 
 	///    a. pass by value
 	///    b. pass by move
 	///    c. pass by reference
@@ -115,5 +217,6 @@ int main()
 	///    a. retrieve by value
 	///    b. retrieve by move
 	///    c. retrieve by reference
+*/
 }
 
