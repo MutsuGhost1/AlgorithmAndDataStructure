711215f03b8b7fa7d3c0911ccbe29aa013b33a07
Li-Wei.Cheng <r93096@csie.ntu.edu.tw>
2021-11-25 09:04:32 +0800

    leetcode: 19. Remove Nth Node From End of List add non-error prone approach

--- Leetcode/19.cpp
+++ Leetcode/19.cpp
@@ -11,6 +11,46 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        return solve2(head, n);
+    }
+    
+    ListNode* solve2(ListNode* head, int n) {
+        ListNode *fast=head, *slow=head;
+        /// guarantee len >= 1
+        /// if(!head) return nullptr;
+        
+        while(0 < n && fast->next) {
+            fast = fast->next;
+            n--;
+        }
+        
+        if(0 < n) {
+            /// when n == length -> delete first element
+            if(1 == n) {
+                head = head->next;
+                return head;     
+            }
+            /// invalid parameters, throw exceptions
+            return head;
+        }
+
+        /// n < length
+        ///
+        /// [1]->[2], n=1
+        while(fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+
+        return head;
+    }
+    
+    /// The navie approach
+    /// 1. use fast/slow pointer
+    /// 2. the slow pointer will go when the fast pointer has run n steps
+    /// ...
+    ListNode* solve1(ListNode* head, int n) {
 /*
         The number of nodes in the list is sz.
           1 <= sz <= 30
@@ -51,6 +91,6 @@ public:
             slow->next = slow->next->next;
         }
         
-        return head;
+        return head;        
     }
 };
