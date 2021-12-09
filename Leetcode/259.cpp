43f233e7a7f8c80194c528bcf5e54925adbdfbe8
Li-Wei.Cheng <r93096@csie.ntu.edu.tw>
2021-12-09 13:18:40 +0800

    leetcode: 259. 3Sum Smaller

--- Leetcode/259.cpp
+++ Leetcode/259.cpp
@@ -1,77 +1,29 @@
-class MedianFinder {
+class Solution {
 public:
-    MedianFinder() {
-    }
+    int threeSumSmaller(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
 
-
-    void addNum(int num) {
-        updateMedian(num);
-    }
-    
-    double findMedian() {
-        return median_;
-    }
-private:
-    
-    /// Time Complexity: O(KN) that K = 5 * 10^4
-    void updateMedian(int num) {
-        /// Time  Complexity: O(N)
-        /// Space Complexity: O(N)
-        insert(num);
-        /// initial
-        if(data_.end() == curr_median_) {
-            curr_median_ = data_.begin();
-            median_ = *curr_median_;
-        } else {
-            ///  before    after
-            ///   odd    -> even
-            ///   3/2= 1    4/2 = 2    -> index of mean will + 1
-            ///   1         2 & 2-1 
-            ///             num <= median -> curr_median, prev(curr_median)
-            ///             num >  median -> curr_median = next(curr_median)
-            ///
-            ///   even   -> odd        -> index of mean will keep the same
-            ///   4/2= 2    5/2 = 2
-            ///
-            ///   2 & 2-1   2
-            ///             num <= median  -> curr_median = prev(curr_median)
-            ///             num >  median  -> curr_median
-            if(size()%2 == 1) {
-                if(num <= *curr_median_)
-                    curr_median_ = prev(curr_median_);
-                median_ = *curr_median_;
-            } else {
-                if(num > *curr_median_)
-                    curr_median_ = next(curr_median_);
-                median_ = (*curr_median_ + *prev(curr_median_)) / 2.0;
+        /*
+             [-2,0,1,3]
+             for(i=0; i<4; i++)
+               left=1, right=3
+               while(left<right)
+                   -2+0+3=1 < 2
+             
+         */
+        
+        int cnt=0;
+        for(int i=0; i<nums.size(); i++) {
+            int left=i+1, right=nums.size()-1;
+            while(left < right) {
+                if(nums[i]+nums[left]+nums[right] < target) {
+                    cnt += (right-left);
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
+        return cnt;
     }
-    
-    list<int>::iterator findInsertPos(int num) {
-        /// always insert the same num in the left
-        for(auto iter=data_.begin(); iter!=data_.end(); iter++)
-            if(num <= *iter)
-                return iter;
-        return data_.end();
-    }
-    
-    void insert(int num) {
-        data_.insert(findInsertPos(num), num);
-    }
-    
-    int size() {
-        return data_.size();
-    }
-    
-    list<int> data_;
-    list<int>::iterator curr_median_ = data_.end();
-    double median_;
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
