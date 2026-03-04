66fccf2391fcd75c0621b8f1886afdb4c96cc65b
Li-Wei Cheng <r93096@csie.ntu.edu.tw>
2026-03-04 13:19:28 +0800

    Update Two Sum Solution

--- Leetcode/cpp/1.cpp
+++ Leetcode/cpp/1.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        /// we can't solve it easily by sorting due to we need return the index before sorting
 
-        int i=0, j=nums.size()-1;
-        while(i <= j) {
-            if(nums[i]+nums[j] > target)
-                j--;
-            else if(nums[i]+nums[j] < target)
-                i++;
-            else
-                return {i, j};
+        /// Use key as value of array
+        /// Use val as index of array
+        unordered_map<int,int> numToIdx;
+        for(int i=0; i<nums.size(); i++) {
+            if(numToIdx.count(target-nums[i])) {
+                return {numToIdx[target-nums[i]], i};
+            }
+            /// Update key and value
+            numToIdx[nums[i]] = i;
         }
+
         return {};
     }
 };
